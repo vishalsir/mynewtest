7e99a4ece45bad746acc634063770af9b6f0e26a
Vishal <Vishal@DESKTOP-DJTUOKQ>
2021-08-20 10:47:13 +0530

    new

---
+++ javabean/src/javabean/Test.java
@@ -0,0 +1,34 @@
+package javabean;
+
+class Employee{
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+		
+}
+
+public class Test {
+
+	public static void main(String[] args) {
+		Employee obj=new Employee();
+		obj.setId(11);
+		obj.setName("Tom");
+		
+		System.out.println("id="+obj.getId());
+		System.out.println("name="+obj.getName());
+		
+	}
+
+}
